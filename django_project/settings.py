4dca907dce27b8d98eb9e54445bd6afbdf5c7134
Harvey Xing <71458491+xing1357@users.noreply.github.com>
2023-04-02 22:13:31 +0000

    pls deploy this time

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "bBkR0DFh4wPUgAFAC7SHRQNqEv55I2fhIxmcg3d3S_U"
 
 if SECRET_KEY is None:
     print(
